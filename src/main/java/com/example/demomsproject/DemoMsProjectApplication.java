95dd838226d19addefd235d5de77bd5823869b49
Giritosh <giritosh@gmail.com>
2023-11-06 12:10:28 +0530

    coderefactor

---
+++ src/main/java/com/example/demomsproject/DemoMsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demomsproject/DemoMsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
